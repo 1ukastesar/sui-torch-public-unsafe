f6fb51df216cdbd6e54ab8e01a3e14c1f91aa22e
Lukas Tesar <lukastesar03@gmail.com>
2025-12-09 13:30:35 +0100

    style: add type hints

--- sui_torch.py
+++ sui_torch.py
@@ -4,11 +4,11 @@ import primitives as p
 
 class Conv1DLayer:
     # Pocitejme s tim, ze stride = 1 a padding = 0.
-    def __init__(self, out_channels, kernel_size):
+    def __init__(self, out_channels: int, kernel_size: int):
         self.kernels = None # Toto budou vahy konvolucni vrstvy
         raise NotImplementedError("Conv1DLayer.__init__ is not implemented")
 
-    def forward(self, x):
+    def forward(self, x: p.Tensor) -> p.Tensor:
         raise NotImplementedError("Conv1DLayer.forward is not implemented")
 
     def parameters(self):
@@ -16,10 +16,10 @@ class Conv1DLayer:
 
 
 class MaxPool1DLayer:
-    def __init__(self, pool_size, stride):
+    def __init__(self, pool_size: int, stride: int):
         raise NotImplementedError("MaxPool1DLayer.__init__ is not implemented")
 
-    def forward(self, x):
+    def forward(self, x: p.Tensor) -> p.Tensor:
         raise NotImplementedError("MaxPool1DLayer.forward is not implemented")
 
     def parameters(self):
